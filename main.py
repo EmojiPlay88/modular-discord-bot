4bb8fe4f21b1c044c482c3cce6955bf1e9633af9
EmojiPlay <76159772+EmojiPlay@users.noreply.github.com>
2025-04-02 16:05:14 +0200

    added raw files

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext.commands import Bot
+from modules.ping import Ping
+
+intents = discord.Intents.all()
+bot = Bot(intents=intents, command_prefix='!')
+global token
+
+with open('token.txt', 'r') as file:
+    token = file.read()
+
+@bot.event
+async def on_ready():
+    print(f'Logged on as {bot.user} (ID: {bot.user.id})')
+
+async def main():
+    await bot.load_extension('modules.ping')
+    await bot.start(token)
+
+#---------------------------------------------------------------------------------
+# Don't forget to enter your bot token in token.txt from Discord Developer Portal!
+#---------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
