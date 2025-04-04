82cfd444776b4e894cfa7d0a126e041b4b01d763
EmojiPlay <76159772+EmojiPlay@users.noreply.github.com>
2025-04-04 08:36:43 +0200

    Add language setting feature and permission checks
    Implemented the /setlanguage command with permission validation and dynamic language file loading. Updated the configuration system to store selected languages and included English translations. Refactored imports and restructured the bot with command handling enhancements.

--- main.py
+++ main.py
@@ -1,14 +1,58 @@
 import discord
 import os
+import asyncio
+import configparser
+import json
+
+from discord import app_commands
 from discord.ext.commands import Bot
 
 global token
+global config
 
+#Defining the bot
 intents = discord.Intents.all()
 bot = Bot(intents=intents, command_prefix='!')
 
+#Initializing the main config file
+config = configparser.ConfigParser()
+config.read('./configs/main.cfg')
+
 with open('token.txt', 'r') as file:
     token = file.read()
+
+def checkPermissions(user:discord.Member, permissions:list):
+    roles = user.roles
+    for role in roles:
+        if role.permissions.value & permissions:
+            return True
+    return False
+
+def getLanguages():
+    availableLanguages = []
+    for dir in os.listdir('./translations'):
+        availableLanguages.append(dir)
+    return availableLanguages
+
+def openLanguageFile(language:str):
+    filename = os.path.basename(__file__)
+    with open(f'./translations/{language}/{filename}.json', 'r') as file:
+        return file.read().json()
+
+class MainCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @discord.app_commands.command(name='setlanguage', description='Sets the language of the bot for the server.')
+    async def setLanguage(self, interaction:discord.Interaction, language: str):
+        prompt = openLanguageFile(language)
+        if not checkPermissions(interaction.user, 1024):
+            await interaction.response(text=prompt['setlang']['noperms'])
+        else:
+            config['language'] = language
+            with open("./configs/main.cfg", "w") as configfile:
+                config.write(configfile)
+            await interaction.response(text=prompt['langchanged']['success'])
 
 @bot.event
 async def on_ready():
@@ -26,5 +70,4 @@ async def main():
 #---------------------------------------------------------------------------------
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
