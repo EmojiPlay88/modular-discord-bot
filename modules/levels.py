2d9b65cdf62890160d05a2368a7d7f104ec90836
Based415 <Diman4ikMythical@mail.ru>
2025-04-07 21:10:00 +0300

    xp and level saving now works

--- modules/levels.py
+++ modules/levels.py
@@ -2,34 +2,47 @@ import discord
 from discord.ext import commands
 import json
 
+def guildcheck(guildid):
+    with open("data/levels.json", 'r',encoding='utf=8') as file:
+        levels = json.load(file)
+        gid = str(guildid)
+    if gid not in levels:
+        levels[gid]= {}
+        with open("data/levels.json", 'w',encoding='utf=8') as f1:
+            json.dump(levels, f1)
+
+def usercheck(guildid, userid):
+    with open("data/levels.json", 'r',encoding='utf=8') as file:
+        levels = json.load(file)
+        gid = str(guildid)
+        uid = str(userid)
+    if uid not in levels[gid]:
+        levels [gid] [uid] = [0,0]
+        with open("data/levels.json", 'w',encoding='utf=8') as f1:
+            json.dump(levels, f1)
+
 class LevelCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
+            
     @commands.hybrid_command(name="level")
     async def level(self,ctx):
-        levels = {}
         userid = ctx.author.id
         guildid = ctx.guild.id
-        with open("data/levels.json", 'r+', encoding='utf-8') as file:
+        guildcheck(guildid)
+        usercheck(guildid,userid)
+        with open("data/levels.json", 'r', encoding='utf-8') as file:
             levels = json.load(file)
-
-            if guildid not in levels:
-                levels [guildid] = {}
-                json.dumps(levels)
-
-            if userid not in levels[guildid]:
-                levels [guildid] [userid] = [0,0]
-                json.dumps(levels)
-
-            json.dump(levels,file)
-
-            lvl = levels[guildid] [userid] [1]
-            xp = levels[guildid] [userid] [0]
-            await ctx.send(f'Your level and xp is{lvl, xp}')
-
+        gid = str(guildid)
+        uid = str(userid)
+        xp = levels[gid] [uid] [0]
+        lvl = levels[gid] [uid] [1]
+        await ctx.send(f'Your xp and level is{xp, lvl}')
+    
 async def setup(bot):
     await bot.add_cog(LevelCommands(bot))
 
 
 #holy fuck help me, i rlly want slep
+#dump or dumps?
+#Where is my mind?
