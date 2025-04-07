02e4032cf62236615cecae1fa269ca5431655702
EmojiPlay <76159772+EmojiPlay@users.noreply.github.com>
2025-04-07 21:02:01 +0200

    Cleaned up levels.py and made it more readable

--- modules/levels.py
+++ modules/levels.py
@@ -1,47 +1,60 @@
 import discord
-from discord.ext import commands
 import json
-from main import bot
+import os
+from discord.ext import commands
 
+#Create the data file if doesn't exist
+def fileCheck():
+    if not os.path.exists("data/levels.json"):
+        with open("data/levels.json", 'w',encoding='utf=8') as file:
+            json.dump({}, file)
+
+#"Simpler" way to load the data file
+def loadJson():
+    try:
+        with open("data/levels.json", 'r',encoding='utf=8') as file:
+            levels = json.load(file)
+            return levels
+    except FileNotFoundError:
+        fileCheck()
+        return loadJson()
+
+#Check if guild id is in the data file
 def guildcheck(guildid):
-    with open("data/levels.json", 'r',encoding='utf=8') as file:
-        levels = json.load(file)
-        gid = str(guildid)
-    if gid not in levels:
-        levels[gid]= {}
-        with open("data/levels.json", 'w',encoding='utf=8') as f1:
-            json.dump(levels, f1)
+    levels = loadJson()
+    if guildid not in levels:
+        levels[guildid]= {}
+        with open("data/levels.json", 'w',encoding='utf=8') as file:
+            json.dump(levels, file)
 
+#Check if user id is in the data file
 def usercheck(guildid, userid):
-    with open("data/levels.json", 'r',encoding='utf=8') as file:
-        levels = json.load(file)
-        gid = str(guildid)
-        uid = str(userid)
-    if uid not in levels[gid]:
-        levels [gid] [uid] = [0,0]
-        with open("data/levels.json", 'w',encoding='utf=8') as f1:
-            json.dump(levels, f1)
+    levels = loadJson()
+    if userid not in levels[guildid]:
+        levels[guildid][userid] = [0,0]
+        with open("data/levels.json", 'w',encoding='utf=8') as file:
+            json.dump(levels, file)
 
 class LevelCommands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-            
+        fileCheck() #On initialization check if data file exists or not
+
+    #/level
+    #TODO: Translation file implementation
     @commands.hybrid_command(name="level")
     async def level(self,ctx):
-        userid = ctx.author.id
-        guildid = ctx.guild.id
+        userid = str(ctx.author.id)
+        guildid = str(ctx.guild.id)
         guildcheck(guildid)
         usercheck(guildid,userid)
         with open("data/levels.json", 'r', encoding='utf-8') as file:
             levels = json.load(file)
-        gid = str(guildid)
-        uid = str(userid)
-        xp = levels[gid] [uid] [0]
-        lvl = levels[gid] [uid] [1]
-        await ctx.send(f'Your xp and level is{xp, lvl}')
+        await ctx.send(f'Your xp and level is{levels[guildid][userid][0], levels[guildid][userid][1]}')
 
-    @bot.event
-    async def on_message(message): ##this not finished (Emoji dont touch it!) 
+    #changed ur @bot.event to @commands.Cog.listener() bc you dont use @bot.event in cogs or whatever it says in docs
+    @commands.Cog.listener()
+    async def on_message(message): ##this not finished (Emoji dont touch it!) Edit: gotcha
         if message.author.bot:
             return
     
@@ -50,3 +63,5 @@ async def setup(bot):
 
 #dump or dumps?
 #Where is my mind?
+
+#copyright gurkinsoft 2025
